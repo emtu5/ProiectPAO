28eacf7b6c3cad129d5c2ede5db7f62a7a8186f8
emtu5 <115076076+emtu5@users.noreply.github.com>
2024-05-30 03:27:01 +0300

    Country Repo JDBC

--- src/Repository/CountryRepository.java
+++ src/Repository/CountryRepository.java
@@ -1,35 +1,78 @@
 package Repository;
 
 import Model.Country;
+import Model.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class CountryRepository implements ICountryRepository {
-    private final SortedSet<Country> countries;
-
-    public CountryRepository() {
-        this.countries = new TreeSet<>();
-    }
     @Override
     public void addCountry(Country country) {
-        countries.add(country);
+        try {
+            Connection conn = DbConstants.getConnection();
+            String insertCountry = "INSERT INTO Country(name) VALUES(?);";
+            PreparedStatement countryStmt = conn.prepareStatement(insertCountry);
+            countryStmt.setString(1, country.getName());
+            countryStmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeCountry(Country country) {
-        countries.remove(country);
+        try {
+            Connection conn = DbConstants.getConnection();
+            String removeCountry = "DELETE FROM Country WHERE name = ?;";
+            PreparedStatement countryStmt = conn.prepareStatement(removeCountry);
+            countryStmt.setString(1, country.getName());
+            countryStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Country getCountryByName(String name) {
-        return countries.stream().filter(country -> country.getName().equals(name)).findFirst().orElse(null);
+        try {
+            Connection conn = DbConstants.getConnection();
+            String findCountry = "SELECT * FROM Country WHERE name = ?;";
+            PreparedStatement countryStmt = conn.prepareStatement(findCountry);
+            countryStmt.setString(1, name);
+            ResultSet rs = countryStmt.executeQuery();
+            if (rs.next()) {
+                return new Country(rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
     @Override
     public SortedSet<Country> getCountries() {
+        try {
+            Connection conn = DbConstants.getConnection();
+            String findCountries = "SELECT * FROM Country;";
+            PreparedStatement countryStmt = conn.prepareStatement(findCountries);
+            ResultSet rs = countryStmt.executeQuery();
+            SortedSet<Country> countries = new TreeSet<Country>();
+            while (rs.next()) {
+                Country country = new Country(rs.getString("name"));
+                countries.add(country);
+            }
+            return countries;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
