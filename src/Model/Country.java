04e7f1a0fed81de1f608bd70bfe5a13401b696df
emtu5 <115076076+emtu5@users.noreply.github.com>
2024-05-30 16:04:18 +0300

    fixed result calculation bug

--- src/Model/Country.java
+++ src/Model/Country.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Objects;
+
 public class Country implements Comparable<Country>{
     private String name;
 
@@ -20,6 +22,18 @@ public class Country implements Comparable<Country>{
         return this.name;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+        return Objects.equals(name, country.name);
+    }
     @Override
     public int compareTo(Country o) {
         return this.name.compareTo(o.name);
--- src/Model/User.java
+++ src/Model/User.java
@@ -37,6 +37,14 @@ public class User implements Comparable<User>{
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) && Objects.equals(email, user.email);
+    }
+
+    @Override
     public int hashCode() {
         return Objects.hash(username, email);
     }
